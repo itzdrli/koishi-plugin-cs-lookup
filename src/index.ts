983ab4ae69e58e2c4d5b590b29555842dee03722
itzdrli <liletian1231@outlook.com>
2024-04-04 22:09:59 +0000

    0.2.2

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ export const Config: Schema<Config> = Schema.object({
 })
 
 export const usage = `
+<h2>如遇使用问题可以前往QQ群: 957500313 讨论<h2>
 <h2> 本插件需要来自 <a href="www.steamwebapi.com">steamwebapi.com</a> 的SteamWebAPI Key 进行非官方接口的背包查询和SteamID查询</h2>
 <h3> 即为: </br>使用官方api查询背包: 不需要key(仅查询背包(中文)且容易被墙)</br>不使用官方api查询背包: 需要key(可以查背包(英文)和SteamID)</h3>
 <p>请我喝杯咖啡 👉<a href="https://ko-fi.com/itzdrli"><img src="https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white" alt="ko-fi"></a></p>
