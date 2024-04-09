14e16dedf08d593fc33b8c39d8e25185b3176e9d
itzdrli <liletian1231@outlook.com>
2024-04-09 18:51:36 +0000

    0.3.0

--- src/index.ts
+++ src/index.ts
@@ -9,19 +9,17 @@ export const inject = {
 }
 export interface Config {
   useSteamAPI: boolean,
-  SteamWebAPIKey: string,
-  useImg: boolean
+  SteamWebAPIKey: string
 }
 
 export const Config: Schema<Config> = Schema.object({
   useSteamAPI: Schema.boolean().default(true).description("是否使用Steam官方API查询 (大陆地区实例容易被墙)"),
   SteamWebAPIKey: Schema.string().description("Steam Web API Key from www.steamwebapi.com"),
-  useImg: Schema.boolean().default(false).description("是否为背包查询(cs-inv)使用图片回复(需要puppeteer)")
 })
 
 export const usage = `
 <h2>如遇使用问题可以前往QQ群: 957500313 讨论<h2>
-<h2> 本插件需要来自 <a href="www.steamwebapi.com">steamwebapi.com</a> 的SteamWebAPI Key 进行非官方接口的背包查询和SteamID查询</h2>
+<h2> 本插件需要来自 <a href="www.steamwebapi.com">steamwebapi.com</a> 的 SteamWebAPI Key 进行非官方接口的背包查询和SteamID查询</h2>
 <h3> 即为: </br>使用官方api查询背包: 不需要key(仅查询背包(中文)且容易被墙)</br>不使用官方api查询背包: 需要key(可以查背包(英文)和SteamID)</h3>
 <p>请我喝杯咖啡 👉<a href="https://ko-fi.com/itzdrli"><img src="https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white" alt="ko-fi"></a></p>
 `;
