de991efba62370b0172ca8b09289b47fb339c905
itzdrli <liletian1231@outlook.com>
2024-04-09 19:29:59 +0000

    0.3.1

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,13 @@ export const inject = {
   optional: ['puppeteer']
 }
 export interface Config {
+  theme: boolean,
   useSteamAPI: boolean,
   SteamWebAPIKey: string
 }
 
 export const Config: Schema<Config> = Schema.object({
+  theme: Schema.boolean().default(false).description('使用浅色主题'),
   useSteamAPI: Schema.boolean().default(true).description("是否使用Steam官方API查询 (大陆地区实例容易被墙)"),
   SteamWebAPIKey: Schema.string().description("Steam Web API Key from www.steamwebapi.com"),
 })
@@ -21,7 +23,7 @@ export const usage = `
 <h2>如遇使用问题可以前往QQ群: 957500313 讨论<h2>
 <h2> 本插件需要来自 <a href="www.steamwebapi.com">steamwebapi.com</a> 的 SteamWebAPI Key 进行非官方接口的背包查询和SteamID查询</h2>
 <h3> 即为: </br>使用官方api查询背包: 不需要key(仅查询背包(中文)且容易被墙)</br>不使用官方api查询背包: 需要key(可以查背包(英文)和SteamID)</h3>
-<p>请我喝杯咖啡 👉<a href="https://ko-fi.com/itzdrli"><img src="https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white" alt="ko-fi"></a></p>
+<p>请我喝杯咖啡 👉<a href="https://ko-fi.com/itzdrli"><img src="https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white" alt="ko-fi"></a></p> <a href="https://afdian.net/a/itzdrli">爱发电</a>
 `;
 
 export function apply(ctx: Context, config: Config) {
