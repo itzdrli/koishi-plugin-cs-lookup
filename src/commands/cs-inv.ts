32a4985322f7b362af39f899fb1894bd30406193
itzdrli <liletian1231@outlook.com>
2024-06-13 19:42:01 +0000

    0.4.5

--- src/commands/cs-inv.ts
+++ src/commands/cs-inv.ts
@@ -1,7 +1,7 @@
 import { Context } from 'koishi'
 import { Config } from '../index'
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import { } from 'koishi-plugin-puppeteer'
 import Umami from '../umami'
 
