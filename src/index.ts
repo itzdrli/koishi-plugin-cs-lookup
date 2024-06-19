a3c776eefc866c0d8d50fe47bee6117a3f92cae6
itzdrli <liletian1231@outlook.com>
2024-06-19 14:12:47 +0000

    0.5.1

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export interface Config {
 }
 
 export const Config: Schema<Config> = Schema.object({
-  data_collect: Schema.boolean().default(true).description('是否允许匿名数据收集  [隐私政策](https://github.com/itzdrli/privacy_policy/blob/main/readme.md)'),
+  data_collect: Schema.boolean().default(true).description('是否允许匿名数据收集 隐私政策见上方链接'),
   theme: Schema.boolean().default(false).description('使用浅色主题'),
   useSteamAPI: Schema.boolean().default(true).description("是否使用Steam官方API查询 (大陆地区实例容易被墙)"),
   SteamWebAPIKey: Schema.string().description("Steam Web API Key from www.steamwebapi.com"),
@@ -24,10 +24,18 @@ export const Config: Schema<Config> = Schema.object({
 export const usage = `
 <h2>如遇使用问题可以前往QQ群: 957500313 讨论<h2>
 <h2> 本插件需要来自 <a href="www.steamwebapi.com">steamwebapi.com</a> 的 SteamWebAPI Key 进行非官方接口的背包查询和SteamID查询</h2>
+<h2> 匿名数据收集 👉 <a href="https://legal.itzdrli.com">隐私政策</a> </h2>
 <h3> 即为: </br>使用官方api查询背包: 不需要key(仅查询背包(中文)且容易被墙)</br>不使用官方api查询背包: 需要key(可以查背包(英文)和SteamID)</h3>
-<p>请我喝杯咖啡 👇</br><a href="https://ko-fi.com/itzdrli"><img src="https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white" alt="ko-fi"></a></p> <a href="https://afdian.net/a/itzdrli">
-  <button>爱发电</button>
-</a>`;
+<p>请我喝杯咖啡 👇</br><a href="https://ko-fi.com/itzdrli"><img src="https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white" alt="ko-fi"></a></p> 
+<a style="
+    font-size: 30px;
+    border: 2px solid red;
+    display: inline-block;
+    border-radius: 20px;
+    padding: 5px;
+    background: blue;
+    color: #fff;
+">爱发电</a>`;
 
 export function apply(ctx: Context, config: Config) {
   inv(ctx, config);
